9f9759630a4a37503b4508c3603808f232d8ff5c
talosrobert <29042337+talosrobert@users.noreply.github.com>
2021-01-27 11:44:09 +0100

    in the mysql role replace storage operations with system roles

--- scripts/adhoc.sh
+++ scripts/adhoc.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-ansible -b -a "shutdown -r now" feds11
-ansible -b -a "shutdown -r now" feds12
+# ansible -b -a "shutdown -r now" feds11
+# ansible -b -a "shutdown -r now" feds12
+
+ansible-playbook $HOME/projects/ansible-practice/playbooks/prep.yml
+ansible-playbook $HOME/projects/ansible-practice/playbooks/update.yml
+ansible-playbook $HOME/projects/ansible-practice/playbooks/selinux.yml
