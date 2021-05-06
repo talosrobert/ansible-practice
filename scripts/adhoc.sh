a101e03c2b4d3eb8daf0c19136553a7cb4010490
talosrobert <29042337+talosrobert@users.noreply.github.com>
2021-05-06 20:47:59 +0200

    update inventory to new env

--- scripts/adhoc.sh
+++ scripts/adhoc.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-# ansible -b -a "shutdown -r now" feds11
-# ansible -b -a "shutdown -r now" feds12
-
-ansible-playbook $HOME/projects/ansible-practice/playbooks/prep.yml
 ansible-playbook $HOME/projects/ansible-practice/playbooks/update.yml
-ansible-playbook $HOME/projects/ansible-practice/playbooks/selinux.yml
+
